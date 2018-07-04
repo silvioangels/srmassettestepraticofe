f9eee1acac4fbf8d1b2947afde3656dd1e33af9a
Silvio Angelo <silvioangels@gmail.com>
2018-07-04 14:48:20 -0300

    Home Formulario

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { ClienteService } from './../../services/cliente.service';
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../model/cliente.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cliente = new Cliente('' , 0 , '');
+  mensagemSucesso: string;
+  mensagemErro: string;
+
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  realizarCadastro() {
+    this.mensagemSucesso = '';
+    this.mensagemErro = '';
+    this.clienteService.realizarCadastro(this.cliente).subscribe(() => {
+      this.router.navigate(['/']);
+      this.mensagemSucesso = 'Cadastro realizado com sucesso';
+    }, err => {
+      this.mensagemErro = 'Ocorreu um erro ao realizar o cadastro';
+    });
   }
 
 }
